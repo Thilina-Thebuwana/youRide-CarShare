53dad3298d31fb3d416fd83248e33ded3f3576ec
christopher.rofique <christopher.rofique@gmail.com>
2018-08-16 22:52:52 +1000

    Booking CSS

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :bookings
+  resources :cars
+  root 'home#index'
+
+   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" ,  registrations: 'reigistrations'}
+   
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
