1277cae6215afa6cdbcfe7e376c705b6f562695a
rmit-s3599177-mehmed-furgan <s3599177@student.rmit.edu.au>
2018-08-16 22:11:41 +1000

    Car Booking

---
+++ app/controllers/reigistrations_controller.rb
@@ -0,0 +1,12 @@
+class ReigistrationsController < Devise::RegistrationsController
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation , :latitude , :longitude, :ip)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+  end
+end
