874f8793fbe6d8e4dfa32ddf5bdc32ec1ad89c04
Thilina S. Thebuwana <thilinasthebuwana@gmail.com>
2018-08-23 23:05:37 +1000

    CSS changes

--- db/schema.rb
+++ db/schema.rb
@@ -55,10 +55,7 @@ ActiveRecord::Schema.define(version: 20180814054642) do
     t.string "uid"
     t.string "first_name"
     t.string "last_name"
-<<<<<<< HEAD
-=======
     t.float "lattitude"
->>>>>>> Connor
     t.float "latitude"
     t.float "longitude"
     t.string "ip"
