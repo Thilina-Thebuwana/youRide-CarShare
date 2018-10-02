eb6edaa346c166c18a0eb8ca671ea56a449e986f
Thilina S Thebuwana <thilinasthebuwana@gmail.com>
2018-10-02 17:29:07 +1000

    Cancel Booking Functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  
-  def authorize_admin
-     redirect_to root_path, alert: 'You dont have access to that page' unless current_user.try(:admin?)
-  	 end
-    
-  def authorize_user_booking
-     redirect_to new_user_session_path, alert: 'You must login in order to make a booking ' unless current_user
-  	 end
-protected
- 
-
-end
-=======
 class ApplicationController < ActionController::Base
   #skip_before_action :verify_authenticity_token
   protect_from_forgery prepend: true
@@ -29,4 +13,3 @@ protected
  
 
 end
->>>>>>> Thilina
